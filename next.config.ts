e880046f1d287c9e6fe7a2ae11400b94dd62aa6f
Akinwalee <obatulafuad@gmail.com>
2026-09-23 17:01:33 +0100

    feat: integrate Sanity CMS, add photoshoot image assets, and configure project settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/journal/in-the-wild-akure",
+        destination: "/in-the-wild",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
